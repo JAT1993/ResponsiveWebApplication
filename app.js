822c1ab5ce48adb7ad017eb9a206d5671b3eea33
JAT1993 <jatinarora.march1993@gmail.com>
2017-06-15 18:35:39 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+ function formValidation(){   var x = document.getElementById('email').value;  var number = document.getElementById('number').value;  console.log(number);  var pos = x.indexOf("@");      var dotpos = x.lastIndexOf(".");        if (pos<1 || dotpos<pos+2 || dotpos+2>=x.length) {           alert("Enter correct email address");    document.getElementById('email').value = "";    document.getElementById('number').value = "";      }else if(number.length!==10){   alert("Phone Number should be of 10 digits");  }else{
+   for(var i = 0; i<number.length ; i++){
+    if(number.charCodeAt(i)<48 || number.charCodeAt(i)>57){     alert("Enter only numbers");     return;    }    }   alert("Welcome "+x);  }
+ }
